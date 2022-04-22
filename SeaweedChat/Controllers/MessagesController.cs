f6933061c13fbc95c3fab039b3987e1e2b2d06aa
Ripretention <r5r44e@gmail.com>
2022-04-22 13:01:58 +0300

    Add possibility to start a chat by username

--- SeaweedChat/Controllers/MessagesController.cs
+++ SeaweedChat/Controllers/MessagesController.cs
@@ -22,18 +22,55 @@ namespace SeaweedChat.Controllers
         [Authorize]
         public async Task<IActionResult> Index()
         {
-            User user = await context.Users.FindAsync(long.Parse(User.Identity.Name));
+            var user = await context.Users.FindAsync(long.Parse(User.Identity.Name));
+            var lastMessages = getLastUserMessages(user);
             ViewData["UserName"] = user.Username;
 
-            var lastMessages = context.Messages
-                .Where(msg => msg.Peer.Id == user.Id)
-                .ToList()
-                .GroupBy(msg => msg.Peer.Id)
-                .Select(groupedMsg => groupedMsg.OrderByDescending(msg => msg.Date).FirstOrDefault())
-                .Where(msg => msg != null)
-                .OrderByDescending(msg => msg.Date)
-                .Take(10);
+            var model = constructChatPreviews(lastMessages);
 
+            return View(model);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Chat(long id)
+        {
+            var user = await context.Users.FindAsync(long.Parse(User.Identity.Name));
+            var lastMessages = getLastUserMessages(user, 40);
+            ViewData["UserName"] = user.Username;
+
+            var lastChatMessages = context.Messages
+               .Where(msg => (msg.Owner.Id == user.Id && msg.Peer.Id == id) || (msg.Peer.Id == user.Id && msg.Owner.Id == id))
+               .OrderByDescending(msg => msg.Date)
+               .Take(50);
+
+            Models.ChatModel model = new Models.ChatModel
+            {
+                Id = id,
+                Messages = lastMessages,
+                Members = lastChatMessages
+                    .Select(msg => new[] { msg.Owner, msg.Peer })
+                    .SelectMany(mbr => mbr)
+                    .Distinct(),
+                ChatPreviews = constructChatPreviews(lastMessages),
+                Title = (await context.Users.FindAsync(id))?.Username ?? "Empty Chat"
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult RedirectToChat(string username)
+        {
+            var user = context.Users.FirstOrDefault(usr => usr.Username == username);
+            return user == null
+                ? RedirectToAction("Index", "Messages")
+                : RedirectToAction("Chat", "Messages", new { Id = user.Id.ToString() });
+        }
+
+        private IEnumerable<Models.ChatPreviewModel> constructChatPreviews(IEnumerable<Message> lastMessages)
+        {
             var model = new List<Models.ChatPreviewModel>();
             foreach (var message in lastMessages)
                 model.Add(new Models.ChatPreviewModel
@@ -44,7 +81,16 @@ namespace SeaweedChat.Controllers
                     isSelected = false
                 });
 
-            return View(model);
+            return model;
         }
+        private IEnumerable<Message> getLastUserMessages(User user, int count = 10) =>
+           context.Messages
+               .Where(msg => msg.Peer.Id == user.Id || msg.Owner.Id == user.Id)
+               .ToList()
+               .GroupBy(msg => msg.Peer.Id == user.Id ? msg.Peer.Id : msg.Owner.Id)
+               .Select(groupedMsg => groupedMsg.OrderByDescending(msg => msg.Date).FirstOrDefault())
+               .Where(msg => msg != null)
+               .OrderByDescending(msg => msg.Date)
+               .Take(Math.Abs(count));
     }
 }
---
+++ SeaweedChat/Models/ChatModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using SeaweedChat.Infrastructure.Models;
+
+namespace SeaweedChat.Models
+{
+    public class ChatModel
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<User> Members { get; set; }
+        public IEnumerable<Message> Messages { get; set; }
+        public IEnumerable<ChatPreviewModel> ChatPreviews { get; set; }
+    }
+}
