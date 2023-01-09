5f64fda70dff7cfb7fe2f830c84371b46af7321e
Ripretention <r5r44e@gmail.com>
2023-01-09 20:23:08 +0300

    Add `Footer` and `Header`

---
+++ SeaweedChat.Web/src/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer color="deep-purple accent-4" absolute>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — <strong>SeaweedChat</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TheFooter",
+});
+</script>
---
+++ SeaweedChat.Web/src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar color="deep-purple accent-4" dense dark>
+    <v-btn icon>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-toolbar-title>SeaweedChat</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-message</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  type: "TheHeader",
+});
+</script>
